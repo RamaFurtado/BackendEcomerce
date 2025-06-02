5e9c7e8fca850dee01a4558b6e6d654693cf0417
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-06-02 08:41:36 -0300

    Camibos en securitpy

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -61,14 +61,14 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
                         .requestMatchers("/api/productos/filtrar").permitAll()
                         .requestMatchers("/api/imagenes/upload").permitAll()
-                        //.requestMatchers("/api/usuarios/**").permitAll()
 
 
                         // Rutas solo para ADMIN (crear, eliminar, actualizar productos)
                         .requestMatchers(HttpMethod.POST, "/api/productos/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/productos/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/productos/**").hasRole("ADMIN")
-                        .requestMatchers("/api/usuarios/cambiar-rol").hasRole("ADMIN")
+                //        .requestMatchers("/api/usuarios/**").hasRole("ADMIN")
+                //        .requestMatchers("/api/usuarios/cambiar-rol").hasRole("ADMIN")
                         .requestMatchers("/api/payments/**").hasRole("ADMIN")
 
 
